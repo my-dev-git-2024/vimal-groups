a6d1b76f5c4fb607354fc1da68ad82ab96916e29
ocean-online-mart <jollytourism2025@gmail.com>
2026-04-10 15:58:02 +0530

    git initaltion

---
+++ asserts/scripts/common.js
@@ -0,0 +1,28 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            gold:     '#c9a84c',
+            'gold-dim':'#7a6228',
+            'gold-light':'#e8c96a',
+            navy:     '#0b0f2a',
+            'navy-2': '#0f1535',
+            'navy-3': '#141b42',
+            'navy-4': '#1a2350',
+            'navy-5': '#1f2a5e',
+            cream:    '#e8e4d4',
+            muted:    '#8892b0',
+          },
+          fontFamily: {
+            display: ['"Cormorant Garamond"', 'serif'],
+            body:    ['"DM Sans"', 'sans-serif'],
+            accent:  ['"Bebas Neue"', 'cursive'],
+            barlow:  ['Barlow', 'sans-serif'],
+          },
+          fontSize: {
+            'ghost': '12rem',
+            'ghost-lg': '18rem',
+          },
+        }
+      }
+    }
